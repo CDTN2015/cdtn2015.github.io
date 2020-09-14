56dc4a78a884e045b7ff2cfa90dd6131a05a7018
CDTN2015 <1512484368@qq.com>
2020-09-14 14:03:27 +0900

    New features

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,44 @@
-let myImage = document.querySelector('img');
-
-let h2Text = document.querySelector('h2');
-
-myImage.onclick = function () {
-    let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/firefox-logo-2017.png') {
-        myImage.setAttribute('src', 'images/firefox-develop-edition.png');
-        h2Text.textContent = 'firefox-develop-edition';
-    }
-    else if (mySrc === 'images/firefox-develop-edition.png') {
-        myImage.setAttribute('src', 'images/firefox-logo-2019.png');
-        h2Text.textContent = 'firefox-logo-2019';
-    }
-    else if (mySrc === 'images/firefox-logo-2019.png') {
-        myImage.setAttribute('src', 'images/firefox-logo-2017.png');
-        h2Text.textContent = 'firefox-logo-2017';
-    }
-}
-
-let myButton = document.querySelector('button');
-//let myHeading = document.querySelector('h1');
+let inputNameButton = document.querySelector('button');
+let welcomeText = document.querySelector('h3');
 function setUserName() {
     let myName = prompt('请输入你的名字。');
-    if(!myName || myName === null) {
-      setUserName();
-    } else {
-      localStorage.setItem('name', myName);
-      myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+    if (!myName || myName === null) {
+        setUserName();
     }
-  }
-if (!localStorage.getItem('name')) {
-    setUserName();
-} 
-else {
+    else {
+        localStorage.setItem('name', myName);
+        welcomeText.innerHTML = 'Welcome, ' + myName;
+        inputNameButton.textContent = 'change your id';
+    }
+}
+if (localStorage.getItem('name')) {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+    welcomeText.textContent = 'Meet you again, ' + storedName;
+    inputNameButton.textContent = 'change your id';
 }
-myButton.onclick = function () {
+inputNameButton.onclick = function () {
     setUserName();
 }
+
+let tempImages = document.getElementById('skins');
+tempImages.onclick = function() {
+    //alert("???");
+    let mySrc = tempImages.getAttribute('src');
+    if (mySrc === '/images/Arknights-summer2.jpg') {
+        tempImages.setAttribute('src', '/images/Arknights-summer1.jpg');
+        tempImages.setAttribute('alt', '明日方舟珊瑚海岸1');
+        tempImages.setAttribute('title', '明日方舟珊瑚海岸1');
+    }
+    else if (mySrc === '/images/Arknights-summer1.jpg') {
+        tempImages.setAttribute('src', '/images/Arknights-new-year.jpg');
+        tempImages.setAttribute('alt', '明日方舟春节');
+        tempImages.setAttribute('title', '明日方舟春节');
+    }
+    else if (mySrc === '/images/Arknights-new-year.jpg') {
+        tempImages.setAttribute('src', '/images/Arknights-summer2.jpg');
+        tempImages.setAttribute('alt', '明日方舟珊瑚海岸2');
+        tempImages.setAttribute('title', '明日方舟珊瑚海岸2');
+    }
+}
+
+
